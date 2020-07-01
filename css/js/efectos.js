10d2201586cb6124aaff81da2e53c915316b0f9f
Jonathan <mcjhona069@gmail.com>
2020-06-30 19:40:10 -0500

    Creacion de Java Script

--- css/js/efectos.js
+++ css/js/efectos.js
@@ -1,27 +1,36 @@
 $(document).ready (function(){
-
-    var acercaDe = $('#acerca-de').offset().top,
-        trabajos = $('#trabajos').offset().top,
-        contacto = $('#contacto').offset().top;
-
-    $('#btn-acerca-de').on('click', function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: acercaDe
-        }, 500);
+    
+        var acercaDe = $('#acerca-de').offset().top,
+            menu = $('#platillos').offset().top,
+            galeria = $('#galeria').offset().top,
+            ubicacion = $('#ubicacion').offset().top;
+    
+        $('#btn-acerca-de').on('click', function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop:  acercaDe - 100
+            }, 500);
+        });
+    
+        $('#btn-menu').on('click', function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: menu
+            }, 500);
+        });
+    
+        $('#btn-galeria').on('click', function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: galeria
+            }, 500);
+        });
+    
+        $('#btn-ubicacion').on('click', function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: ubicacion
+            }, 500);
+        });
+    
     });
-
-    $('#btn-trabajos').on('click', function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: trabajos
-        }, 500);
-    });
-
-    $('#btn-contacto').on('click', function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: contacto
-        }, 500);
-    });
-});
